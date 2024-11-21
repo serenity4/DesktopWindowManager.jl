649f3e7360fd78312e105f8cdb18ac229378feae
serenity4 <cedric.bel@hotmail.fr>
2024-11-21 21:18:10 +0100

    Use 2 spaces for tabs in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,15 @@ using Test
 
 # https://learn.microsoft.com/en-us/windows/win32/learnwin32/your-first-windows-program
 @testset "DesktopWindowManager.jl" begin
-    DWM.is_supported() || return
+  DWM.is_supported() || return
 
-    wm = WindowManager()
-    window = Window(wm; window_title = "Test Window")
-    was_visible = map_window(window)
-    @test !was_visible
-    was_visible = unmap_window(window)
-    @test was_visible
-    map_window(window)
-    queue = EventQueue(wm)
-    collect_events!(queue)
+  wm = WindowManager()
+  window = Window(wm; window_title = "Test Window")
+  was_visible = map_window(window)
+  @test !was_visible
+  was_visible = unmap_window(window)
+  @test was_visible
+  map_window(window)
+  queue = EventQueue(wm)
+  collect_events!(queue)
 end;
